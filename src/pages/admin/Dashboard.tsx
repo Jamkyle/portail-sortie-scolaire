8f8c29b3f180f56f990e7c242e863ab0c0c3fe08
Aro Rahari <arorahari@Aros-MacBook-Pro.local>
2024-06-17 12:30:25 +0200

    get list users from dashboard admin

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,7 +1,45 @@
+import { useContext, useEffect, useState } from "react";
+import api from "@/config/api.json";
+import { UserContext } from "@/context/user/User";
+
+type UserList = UserResponse[];
+type UserResponse = {
+  "@id": string;
+  "@type": string;
+  email: string;
+  id: number;
+  roles: string[];
+};
 const DashBoard = () => {
+  const { user } = useContext(UserContext);
+  const [usersList, setUsersList] = useState<UserList>([]);
+  useEffect(() => {
+    fetch(`${api.url}/users`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user?.token}`,
+      },
+    })
+      .then((resp) => resp.json())
+      .then((json) => {
+        setUsersList(json?.["hydra:member"]);
+      })
+      .catch((err) => console.log(err));
+  }, [user]);
+
   return (
     <>
-      <h1>DashBoard Admin</h1>
+      <h3 className="text-4xl font-bold text-center text-amber-200">List des membres</h3>
+      <ul>
+        {usersList.map((user) => {
+          return (
+            <li key={user["@id"] + user["@type"]}>
+              <span>{user.email}</span>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
