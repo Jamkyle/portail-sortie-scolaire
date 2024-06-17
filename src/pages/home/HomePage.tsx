0e8c36e1a85f8b4ebd99bde67d6127f0eb367a0f
Aro Rahari <arorahari@Aros-MacBook-Pro.local>
2024-06-17 11:42:37 +0200

    add nav to login

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -4,9 +4,12 @@ import "@/App.css";
 
 const HomePage = () => {
   return (
-    <div className="bg-teal-600">
-      <Link to="/registration">
+    <div className="bg-teal-600 flex flex-col">
+      <Link to="/registration" className="text-white py-2 px-4 rounded-lg">
         Formulaire d'inscription
+      </Link>
+      <Link to="/login" className="text-white py-2 px-4 rounded-lg">
+        Connexion
       </Link>
     </div>
   );
